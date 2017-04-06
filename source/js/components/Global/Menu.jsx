cf163ae8d764a9be854ea5ff06de2f107355b685
Artem Kubariev <artem_kubariev@epam.com>
2017-04-06 16:04:21 +0300

    Webpack + react

---
+++ source/js/components/Global/Menu.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { IndexLink, Link } from 'react-router';
+import { routeCodes } from '../../routes';
+
+export default class Menu extends Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
